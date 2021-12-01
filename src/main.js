f5aff442639134e400df1ff525c5cf02c0275427
Salman Prottoy <salman.prottoy@gmail.com>
2021-12-01 18:19:21 +0600

    vue bootstrap installed

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
@@ -10,6 +15,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
