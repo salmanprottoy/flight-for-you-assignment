f2feb5afd922d463facb2e57791ec75ef19f7e77
Salman Prottoy <salman.prottoy@gmail.com>
2021-12-03 13:02:50 +0600

    flight UI updated

--- src/components/Flights.vue
+++ src/components/Flights.vue
@@ -2,9 +2,9 @@
   <div class="m-2 p-2">
     <b-tabs content-class="mt-3 font-weight-bold">
       <b-tab title="Flights" class="text-dark" active>
-        <div class="row" id="fight-search">
-          <div class="col-md-3">
-            <b-card class="text-center" id="fight-search-card">
+        <div class="row g-1" id="fight-search">
+          <div class="col-md-3 shadow">
+            <b-card class="text-center rounded-0" id="fight-search-card">
               <b-card-text>
                 <p>Leaving From</p>
                 <h1>CDS</h1>
@@ -12,8 +12,8 @@
               </b-card-text>
             </b-card>
           </div>
-          <div class="col-md-3">
-            <b-card class="text-center" id="fight-search-card">
+          <div class="col-md-3 shadow">
+            <b-card class="text-center rounded-0" id="fight-search-card">
               <b-card-text>
                 <p>Going To</p>
                 <h1>CDS</h1>
@@ -21,8 +21,8 @@
               </b-card-text>
             </b-card>
           </div>
-          <div class="col-md-3">
-            <b-card class="text-center" id="fight-search-card">
+          <div class="col-md-3 border-3 shadow">
+            <b-card class="text-center rounded-0" id="fight-search-card">
               <b-card-text
                 >Departing on
                 <h3>2021-10-10</h3>
@@ -34,10 +34,76 @@
               </b-card-text>
             </b-card>
           </div>
-          <div class="col-md-3">
-            <b-button variant="success" size="lg" class="w-100 h-100">
+          <div class="col-md-3 shadow">
+            <button size="lg" class="btn btn-grad w-100 h-100 rounded-0">
+              <h1>
+                <font-awesome-icon icon="plane" />
+              </h1>
               <h2>Search Flight</h2>
-            </b-button>
+            </button>
+          </div>
+        </div>
+
+        <div class="row mt-3 g-1">
+          <div class="col-md-3 shadow">
+            <b-card
+              class="text-center rounded-0 w-100 h-100"
+              border-variant="light"
+              text-variant="white"
+              id="flight-card"
+            >
+              <b-card-text>
+                <h1>
+                  <font-awesome-icon icon="list" />
+                </h1>
+                <h2>Manage Booking</h2>
+              </b-card-text>
+            </b-card>
+          </div>
+          <div class="col-md-3 shadow">
+            <b-card
+              class="text-center rounded-0 w-100 h-100"
+              border-variant="light"
+              text-variant="white"
+              id="flight-card"
+            >
+              <b-card-text>
+                <h1>
+                  <font-awesome-icon icon="ticket-alt" />
+                </h1>
+                <h2>Check in</h2>
+              </b-card-text>
+            </b-card>
+          </div>
+          <div class="col-md-3 shadow">
+            <b-card
+              class="text-center rounded-0 w-100 h-100"
+              border-variant="light"
+              text-variant="white"
+              id="flight-card"
+            >
+              <b-card-text>
+                <h1>
+                  <font-awesome-icon icon="exchange-alt" />
+                </h1>
+                <h2>Rebook and Refund</h2>
+              </b-card-text>
+            </b-card>
+          </div>
+          <div class="col-md-3 shadow">
+            <b-card
+              class="text-center rounded-0 w-100 h-100"
+              border-variant="light"
+              text-variant="white"
+              id="flight-card"
+            >
+              <b-card-text>
+                <h1>
+                  <font-awesome-icon icon="exclamation-triangle" />
+                </h1>
+                <h2>Covid-19 Update</h2>
+              </b-card-text>
+            </b-card>
           </div>
         </div>
       </b-tab>
@@ -64,5 +130,60 @@ export default {
 }
 #fight-search-card {
   min-height: 100%;
+  background: linear-gradient(whitesmoke, rgba(245, 245, 245, 0.904)),
+    linear-gradient(to right, #005d63, #6cc2c9d7);
+  border: 5px solid transparent;
+  background-repeat: no-repeat;
+  background-origin: padding-box, border-box;
+}
+#flight-card {
+  background-image: linear-gradient(
+    to right,
+    #005d63 0%,
+    #005c63af 51%,
+    #005d63 100%
+  );
+}
+
+#flight-card {
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0 0 20px rgba(238, 238, 238, 0.603);
+  border-radius: 10px;
+  display: block;
+}
+
+#flight-card:hover {
+  background-position: right center;
+  color: #fff;
+  text-decoration: none;
+}
+
+.btn-grad {
+  background-image: linear-gradient(
+    to right,
+    #005d63 0%,
+    #005c63af 51%,
+    #005d63 100%
+  );
+}
+.btn-grad {
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0 0 20px #eee;
+  border-radius: 10px;
+  display: block;
+}
+
+.btn-grad:hover {
+  background-position: right center;
+  color: #fff;
+  text-decoration: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,18 @@ import App from "./App";
 import BootstrapVue from "bootstrap-vue/dist/bootstrap-vue.esm";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faPlane,
+  faList,
+  faTicketAlt,
+  faExchangeAlt,
+  faExclamationTriangle
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faPlane, faList, faTicketAlt, faExchangeAlt, faExclamationTriangle);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
